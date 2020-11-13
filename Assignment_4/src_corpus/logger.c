f0275ad5ecf711a030736c849350350cc556eb0f
ChristosTrimas <christos.trimas@gmail.com>
2020-11-13 15:21:55 +0200

    started a library and the log entry function

--- Assignment_4/src_corpus/logger.c
+++ Assignment_4/src_corpus/logger.c
@@ -1,16 +1,93 @@
 #define _GNU_SOURCE
 
-#include <time.h>
-#include <stdio.h>
-#include <dlfcn.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <openssl/md5.h>
+#include "myLib.h"
 
-FILE *
-fopen(const char *path, const char *mode) 
+void log_entry(const char* path, unsigned const char accessible)
+{
+	FILE* (*_fopen)(const char*, const char*);
+	FILE* fp;
+
+	char ttime[80], tdate[80], abspath[1024], logbuff[1024];
+
+	struct stat statinfo;
+	struct tm* timeInfo;
+	time_t pure_time;
+
+	unsigned long i = 0;
+	FILE* lp;
+	char buffer[1024];
+	unsigned char hash[MD5_DIGEST_LENGTH];
+	int md5count;
+	MD5_CTX md_ctx;
+
+	_fopen = dlsym(RTLD_NEXT, "fopen");
+
+	//get the full path
+	realpath(path, abspath);
+
+	//get time
+	time(&pure_time);
+	timeInfo = localtime(&pure_time);
+
+	strftime(ttime, 80, "%T", timeInfo);
+	strftime(tdate, 80, "%F", timeInfo);
+
+	sprintf(logbuff, "UID\tfile_name");
+
+	for(; i < strlen(abspath)/8; i++)
+	{
+		sprintf(logbuff + strlen(logbuff), "\t");
+	}
+
+	sprintf(logbuff + strlen(logbuff), "date\t\ttime\t\topen\taction_denied\thash\n");
+	sprintf(logbuff + strlen(logbuff), "%d\t%s\t%s\t%s\t%d\t%d\t\t",(unsigned int)getuid(), abspath, tdate, ttime, accessible, -1 * stat(abspath, &statinfo));
+
+	lp = _fopen(abspath, "rb");
+
+	if(lp)
+	{
+		MD5_Init(&md_ctx);
+
+		while((md5count = fread(buffer, 1, 1024, lp)))
+		{
+			MD5_Update(&md_ctx, buffer, md5count);
+		}
+
+		MD5_Final(hash, &md_ctx);
+
+		for(md5count = 0; md5count < MD5_DIGEST_LENGTH; md5count++)
+		{
+			sprintf(logbuff + strlen(logbuff), "%02x", hash[md5count]);
+		}
+	}
+	else
+	{
+		sprintf(logbuff + strlen(logbuff), "(none)");
+	}
+
+	fclose(lp);
+
+	sprintf(logbuff + strlen(logbuff), "\n");
+
+	fp = _fopen("log.txt", "a");
+
+	if(!fp)
+	{
+		sprintf(logbuff + strlen(logbuff), "log.txt could not be open.\n");
+		return;
+	}
+
+	fputs(logbuff, fp);
+	fclose(fp);	
+
+}
+
+
+
+
+
+
+FILE* fopen(const char *path, const char *mode) 
 {
 
 	FILE *original_fopen_ret;
@@ -20,20 +97,11 @@ fopen(const char *path, const char *mode)
 	original_fopen = dlsym(RTLD_NEXT, "fopen");
 	original_fopen_ret = (*original_fopen)(path, mode);
 
-
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
-
-
 	return original_fopen_ret;
 }
 
 
-size_t 
-fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) 
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) 
 {
 
 	size_t original_fwrite_ret;
@@ -42,14 +110,6 @@ fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 	/* call the original fwrite function */
 	original_fwrite = dlsym(RTLD_NEXT, "fwrite");
 	original_fwrite_ret = (*original_fwrite)(ptr, size, nmemb, stream);
-
-
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
-
 
 	return original_fwrite_ret;
 }
---
+++ Assignment_4/src_corpus/myLib.h
@@ -0,0 +1,37 @@
+#ifndef MY_LIB_H
+#define MY_LIB_H
+
+#include <time.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <openssl/md5.h>
+
+// the fopen and fwrite functions of the logger.c file
+
+FILE* fopen(const char *, const char *);
+
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+
+// my functions
+
+/**********************************************************************
+* This function creates and stores all the necessary information for  *
+* each log entry. UID, File_name, Date, Timestamp, Access type,       *
+* Is action denied flag, File fingerprint 							  *
+***********************************************************************/
+
+void log_entry(const char*, unsigned const char );
+
+/**********************************************************************
+* This function creates and stores all the necessary information for  *
+* each log entry. UID, File_name, Date, Timestamp, Access type,       *
+* Is action denied flag, File fingerprint 							  *
+***********************************************************************/
+
+void flog_entry(FILE* , unsigned const char );
+
+#endif
