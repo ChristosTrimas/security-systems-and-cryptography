be29e44d469ecdaf75c5f40c74a39248b4975efc
ChristosTrimas <christos.trimas@gmail.com>
2020-12-14 22:45:25 +0200

    first commit for ass6

---
+++ Assignment_6/netMonitor.h
@@ -0,0 +1,19 @@
+#ifndef NETMONITOR_H
+#define NETMONITOR_H
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <openssl/md5.h>
+#include <stdbool.h>
+// #include <pcap.h>
+#include <pcap/pcap.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+
+#endif
